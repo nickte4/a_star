0c3c64cae46247b549d4a83db5247b6591685daa
nickte4 <ntopacio25@gmail.com>
2023-04-11 13:02:22 -0400

    Added proper class definition for Spot

--- priorityQueue.js
+++ priorityQueue.js
@@ -2,3 +2,9 @@
  * File for implementing a priority queue
  * using an array implementation of binary min-heap
  */
+class pQueue {
+  constructor(size) {
+    var heap = Array(size).fill(0);
+    this.size = -1;
+  }
+}
--- spot.js
+++ spot.js
@@ -10,32 +10,31 @@ class Spot {
     this.neighbors = []; // all neighbors of spot
     this.previous = undefined; // pointer to where it came from
     this.wall = false; // marks if it is a wall
-
     // decimal == chance to be a wall (e.g. 0.1 = 10% chance)
     if (random(1) < 0.3) {
       this.wall = true;
     }
+  }
 
-    this.show = function (spotColor) {
-      fill(spotColor);
-      if (this.wall) fill(0);
-      noStroke();
-      rect(this.i * w, this.j * h, w - 1, h - 1);
-    };
+  show(spotColor) {
+    fill(spotColor);
+    if (this.wall) fill(0);
+    noStroke();
+    rect(this.i * w, this.j * h, w - 1, h - 1);
+  }
 
-    this.addNeighbors = function (grid) {
-      var i = this.i;
-      var j = this.j;
-      // all neighbors of pixel on each side
-      if (i < cols - 1) this.neighbors.push(grid[i + 1][j]);
-      if (i > 0) this.neighbors.push(grid[i - 1][j]);
-      if (j < rows - 1) this.neighbors.push(grid[i][j + 1]);
-      if (j > 0) this.neighbors.push(grid[i][j - 1]);
-      // all neighbors of pixel on its corners
-      if (i > 0 && j > 0) this.neighbors.push(grid[i - 1][j - 1]);
-      if (i < cols - 1 && j > 0) this.neighbors.push(grid[i + 1][j - 1]);
-      if (i > 0 && j < rows - 1) this.neighbors.push(grid[i - 1][j + 1]);
-      if (i < cols - 1 && j < rows - 1) this.neighbors.push(grid[i + 1][j + 1]);
-    };
+  addNeighbors(grid) {
+    var i = this.i;
+    var j = this.j;
+    // all neighbors of pixel on each side
+    if (i < cols - 1) this.neighbors.push(grid[i + 1][j]);
+    if (i > 0) this.neighbors.push(grid[i - 1][j]);
+    if (j < rows - 1) this.neighbors.push(grid[i][j + 1]);
+    if (j > 0) this.neighbors.push(grid[i][j - 1]);
+    // all neighbors of pixel on its corners
+    if (i > 0 && j > 0) this.neighbors.push(grid[i - 1][j - 1]);
+    if (i < cols - 1 && j > 0) this.neighbors.push(grid[i + 1][j - 1]);
+    if (i > 0 && j < rows - 1) this.neighbors.push(grid[i - 1][j + 1]);
+    if (i < cols - 1 && j < rows - 1) this.neighbors.push(grid[i + 1][j + 1]);
   }
 }
